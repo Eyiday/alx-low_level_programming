8397c1572a53ceecc696ee66222565d909581a01
eyiday <eyiaroadedayo@gmail.com>
2022-09-27 07:25:36 -0700

    learning c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-/**
- *
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
+int main () //learning c
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return 0;
 }
